f59509af7705f6a3df642b786995d954b87088a5
Alexander Leidinger <Alexander@Leidinger.net>
2023-11-17 11:00:31 +0100

    sync with lame-svn as of 2023-11-17

---
+++ webpages/contact.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <title>LAME MP3 Encoder :: Contact Information</title>
+    <meta name="author" content="Roberto Amorim - rjamorim@yahoo.com" />
+    <meta name="generator" content="jEdit 5.4" />
+    <meta name="cvs-version" content="$Id: contact.php,v 1.9 2017-08-03 12:33:12 rjamorim Exp $" />
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <link rel="stylesheet" type="text/css" href="styles/lame.css" />
+    <!--[if IE 6]>
+    <link rel="stylesheet" type="text/css" href="styles/ie.css" />
+    <![endif]-->
+</head>
+<body>
+
+<?php include("menu.html") ?>
+
+<div id="container">
+<div id="content">
+
+<div align="center">
+    <img src="images/logo.gif" width="358" height="231" alt="LAME Official Logo" />
+    <h1>Contact Information</h1>
+</div>
+
+<h3>MP3 encoder mailing list</h3>
+
+<p>
+    For general discussions and comments about LAME and MP3 encoding there used
+    to be the <a href="list.php">MP3 encoder mailing list</a>. Unfortunately it
+    is now gone, although you can still access the list archives in the links
+    below:
+</p>
+
+<p>
+    <ul>
+       <li>Archives for <a
+           href="http://www.mail-archive.com/mp3encoder%40minnie.tuhs.org/">Sept
+           2001 - Sept 2011</a>
+       </li>
+       <li>Archives for <s><a
+           href="http://www.mail-archive.com/mp3encoder%40minnie.cs.adfa.edu.au/">Oct
+           2000 - Sept 2001</a></s>
+       </li>
+       <li>Archives for <a
+           href="http://www.mail-archive.com/mp3encoder%40geek.rcc.se">before
+           Oct 2000</a>
+       </li>
+    </ul>
+</p>
+
+<h3>LAME development mailing list</h3>
+
+<p>
+    <a href="https://sourceforge.net/projects/lame/lists/lame-dev">The lame-dev
+    list</a> is the right place for everything related to LAME development: bug
+    reports, questions and suggestions concerning the encoder, the documentation
+    and the web pages. It is also the best way to get in touch with the active
+    developers. <a href="https://sourceforge.net/p/lame/mailman/lame-dev/">
+    Archives</a> are provided by SourceForge.
+</p>
+
+<h3>SVN mailing list</h3>
+
+<p>
+    If you want to be automatically notified of commits to SVN, you can subscribe
+    to <a href="https://sourceforge.net/projects/lame/lists/lame-cvs">the lame-cvs
+    list</a>. The name of this list has historic reasons, we have not renamed it
+    after switching from CVS to SVN.
+</p>
+
+<h3>IRC</h3>
+
+<p>
+    Some developers and other interested parties can eventually be found on the
+    #mp3encoder channel on the <a href="http://freenode.net/">FreeNode
+    Network</a>.
+</p>
+
+<h3>Bug reports/tracker</h3>
+
+<p>
+    To submit bug reports, you can use the <a
+    href="https://sourceforge.net/p/lame/bugs/">bug tracker</a> besides the <a
+    href="https://sourceforge.net/projects/lame/lists/lame-dev">lame-dev mailing
+    list</a>. Please be descriptive in your bug report. You should clearly
+    mention your problem, LAME version used and how to reproduce the bug.
+</p>
+
+<h3>Contact developers</h3>
+
+<p>
+    Here is the list of <a href="developers.php">developers</a>. If you encounter
+    a problem, please use any of the ways mentioned above instead of directly
+    contacting a developer.
+</p>
+
+<h3>Contact the webmaster</h3>
+
+<p>
+    The current webmaster for the LAME project is Roberto Amorim. You can
+    contact him through <a href="mailto:rjamorim@users.sourceforge.net">e-mail</a>.
+</p>
+
+</div>
+<?php include("footer.html") ?>
+</div>
+
+</body>
+</html>
---
+++ webpages/using.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	<title>LAME MP3 Encoder :: Using LAME</title>
+	<meta name="author" content="Roberto Amorim - rjamorim@yahoo.com" />
+	<meta name="generator" content="jEdit 5.4" />
+	<meta name="cvs-version" content="$Id: using.php,v 1.7 2009-11-03 16:11:01 rjamorim Exp $" />
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+	<link rel="stylesheet" type="text/css" href="styles/lame.css" />
+	<!--[if IE 6]>
+	<link rel="stylesheet" type="text/css" href="styles/ie.css" />
+	<![endif]-->
+</head>
+<body>
+
+<?php include("menu.html") ?>
+
+<div id="container">
+<div id="content">
+
+<div align="center">
+	<img src="images/logo.gif" width="358" height="231" alt="LAME Official Logo" />
+	<h1>Using LAME</h1>
+</div>
+
+<ul>
+	<li>
+		<a href="download.php">Download the source code (SVN, .tar and source
+			RPMS available).</a>
+	</li>
+	
+	<li>
+		<a href="links.php">Software which uses LAME</a>
+	</li>
+	
+	<li>
+		<a href="links.php">MP3 Related Links</a>
+	</li>
+	
+	<li>
+		<a href="license.txt">Can I use LAME in non-open source software?</a>
+	</li>
+	
+	<li>
+		<a href="lame_ui_example.php">User interface guidelines</a>, if you wish
+		to use LAME in your program.
+	</li>
+</ul>
+
+<h3>Documentation</h3>
+
+<ul>
+	<li>
+		<a href="https://svn.code.sf.net/p/lame/svn/trunk/lame/USAGE">Guide 
+		to command line options (in SVN)</a>
+	</li>
+	
+	<li>
+		<a href="tech-FAQ.txt">Technical FAQ</a>
+	</li>
+	
+	<li>
+		<a
+		href="https://svn.code.sf.net/p/lame/svn/trunk/lame/doc/html/index.html">Program
+		documentation (in SVN)</a>
+	</li>
+</ul>
+
+</div>
+<?php include("footer.html") ?>
+</div>
+
+</body>
+</html>
